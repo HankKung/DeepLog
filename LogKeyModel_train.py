81a6d9f88dd0f2eb14ea921ff1e6db4e36a992b1
Hank Kung <hank910140@gmail.com>
2020-05-26 17:25:37 +0800

    attention model

--- LogKeyModel_train.py
+++ LogKeyModel_train.py
@@ -34,14 +34,49 @@ class Model(nn.Module):
         self.hidden_size = hidden_size
         self.num_layers = num_layers
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.attention = Attention(hidden_size)
         self.fc = nn.Linear(hidden_size, num_keys)
 
     def forward(self, x):
         h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
         c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
-        out, _ = self.lstm(x, (h0, c0))
+        out, (final_hidden_state, final_cell_state) = self.lstm(x, (h0, c0))
+        out = output.permute(1, 0, 2)
+        out = self.attention_net(out, final_hidden_state)
         out = self.fc(out[:, -1, :])
         return out
+
+def attention_net(self, lstm_output, final_state):
+
+        """ 
+        Now we will incorporate Attention mechanism in our LSTM model. In this new model, we will use attention to compute soft alignment score corresponding
+        between each of the hidden_state and the last hidden_state of the LSTM. We will be using torch.bmm for the batch matrix multiplication.
+        
+        Arguments
+        ---------
+        
+        lstm_output : Final output of the LSTM which contains hidden layer outputs for each sequence.
+        final_state : Final time-step hidden state (h_n) of the LSTM
+        
+        ---------
+        
+        Returns : It performs attention mechanism by first computing weights for each of the sequence present in lstm_output and and then finally computing the
+                  new hidden state.
+                  
+        Tensor Size :
+                    hidden.size() = (batch_size, hidden_size)
+                    attn_weights.size() = (batch_size, num_seq)
+                    soft_attn_weights.size() = (batch_size, num_seq)
+                    new_hidden_state.size() = (batch_size, hidden_size)
+                      
+        """
+        
+        hidden = final_state.squeeze(0)
+        attn_weights = torch.bmm(lstm_output, hidden.unsqueeze(2)).squeeze(2)
+        soft_attn_weights = F.softmax(attn_weights, 1)
+        new_hidden_state = torch.bmm(lstm_output.transpose(1, 2), soft_attn_weights.unsqueeze(2)).squeeze(2)
+        
+        return new_hidden_state
 
 
 if __name__ == '__main__':
